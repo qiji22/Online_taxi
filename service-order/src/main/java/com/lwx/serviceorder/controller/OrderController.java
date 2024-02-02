7a583c98ac6b9decc4d2394d73f1bbcddfd3ab52
qiji22 <129868968+qiji22@users.noreply.github.com>
2024-02-02 09:46:14 +0800

    swagger

--- service-order/src/main/java/com/lwx/serviceorder/controller/OrderController.java
+++ service-order/src/main/java/com/lwx/serviceorder/controller/OrderController.java
@@ -3,6 +3,10 @@ package com.lwx.serviceorder.controller;
 import com.lwx.internalcommon.dto.ResponseResult;
 import com.lwx.internalcommon.request.OrderRequest;
 import com.lwx.serviceorder.service.IOrderService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
  */
 @RestController
 @RequestMapping("/order")
+@Api(tags = "订单")
 public class OrderController {
 
     @Autowired
@@ -22,6 +27,11 @@ public class OrderController {
 
 
     @PostMapping("/add")
+    @ApiOperation(value = "添加")
+    @ApiImplicitParams({
+            @ApiImplicitParam(name = "page", value = "分页起始位置", required = true, dataType = "int"),
+            @ApiImplicitParam(name = "limit", value = "分页结束位置", required = true, dataType = "int")
+    })
     public ResponseResult saveOrder(@RequestBody OrderRequest orderRequest){
         return iOrderService.saveOrder(orderRequest);
     }
